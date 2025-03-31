79ebc0b1802b18b1372e3517888c589c8196b77a
Christopher Kelley <christopher@cktech.org>
2025-03-30 23:35:00 -0400

    fixing issues with nvim Color scheme

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -18,10 +18,29 @@ require("lazy").setup({
 
 -- Basic UI
 vim.o.termguicolors = true
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("tokyonight")
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = "a"
+vim.opt.background = "dark"
+vim.cmd.colorscheme("tokyonight")
+
+-- Set custom highlight groups for dark blues, minty greens, and comments
+vim.api.nvim_set_hl(0, "Normal", { fg = "#7aa2f7", bg = "none" })         -- Light Blue for main text (Normal)
+vim.api.nvim_set_hl(0, "Comment", { fg = "#7aa2f7", italic = true })      -- Light Blue for comments, italic
+vim.api.nvim_set_hl(0, "Constant", { fg = "#7aa2f7", bg = "none" })       -- Light Blue constants
+vim.api.nvim_set_hl(0, "String", { fg = "#66d9ef", bg = "none" })         -- Teal for strings
+vim.api.nvim_set_hl(0, "Function", { fg = "#88aff0", bold = true })       -- Mint green for functions
+vim.api.nvim_set_hl(0, "Identifier", { fg = "#7aa2f7", bg = "none" })     -- Light Blue Identifiers
+vim.api.nvim_set_hl(0, "Keyword", { fg = "#9a0ade", bold = true })        -- Purple for keywords
+vim.api.nvim_set_hl(0, "Operator", { fg = "#7aa2f7", bg = "none" })       -- Light Blue Operators
+
+
+
+
+-- Keywords and keywords functions
+vim.api.nvim_set_hl(0, "Keyword", { fg = "#9a0ade", bold = true })        -- Purple for keywords
+vim.api.nvim_set_hl(0, "Operator", { fg = "#57c7ff", bg = "none" })       -- Light Blue Operators
 
 -- Telescope keymap
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { noremap = true })
