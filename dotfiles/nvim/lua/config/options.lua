12fca4d82b85a9b3b6543c2b3c1ccdf38ed3f455
GhostKellz <ckelley@ghostkellz.sh>
2025-06-05 18:41:31 -0400

    Refactored init.lua for new plugins and auto indetation

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,5 +1,10 @@
-return {
-  colorscheme = "tokyonight"
-}
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.cmd.colorscheme("tokyonight")  
 
--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  cmd = 'Telescope',
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = vim.fn.executable("make") == 1,
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  cmd = "Telescope",
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep"  },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers"    },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags"  },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
   },
   config = function()
-    require('telescope').setup({
+    require("telescope").setup({
       defaults = {
-        layout_strategy = 'vertical',
+        layout_strategy = "vertical",
         layout_config = { height = 0.9 },
+        sorting_strategy = "ascending",
+        file_sorter = require("telescope.sorters").get_fzf_sorter,
+        generic_sorter = require("telescope.sorters").get_generic_fzf_sorter,
       },
     })
   end,
