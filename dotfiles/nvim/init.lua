20f0729e6344d8a993253be07ae1ba8480109e1b
GhostKellz <ckelley@ghostkellz.sh>
2025-06-05 15:52:45 -0400

    Updated Leader Keymapping

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,9 +1,17 @@
--- Bootstrap Lazy.nvim 
+-- GhostKellz Lazy.vim
+-- Set Leader keys FIRST
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- Leader keymaps 
+vim.g.mapleader = " "         -- Spacebar as the leader key
+vim.g.maplocalleader = "\\"   -- Optional: local leader
 
 -- Plugins
 require("lazy").setup({
