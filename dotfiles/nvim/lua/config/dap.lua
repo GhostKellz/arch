c9fbd4cea697194aad8a7239313f4b87936fb1eb
GhostKellz <ckelley@ghostkellz.sh>
2025-06-07 06:30:46 -0400

    DotShot Sync: Added nvim healthcheck fix script

---
+++ dotfiles/nvim/lua/config/dap.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Start Debug" })
+vim.keymap.set("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Step Over" })
+vim.keymap.set("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Step Into" })
+vim.keymap.set("n", "<F12>", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Step Out" })
+vim.keymap.set("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Toggle Breakpoint" })
